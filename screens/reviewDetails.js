bd42fa3aa1135c6fb05a129555013e69c62a2fa1
sebgatullah372 <sebgatullah.cuet.cse14@gmail.com>
2021-05-20 03:08:39 +0600

    Using images and background images

--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -1,17 +1,40 @@
-import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Button, Image, ImageBackground } from 'react-native';
 import globalStyles from '../styles/globalStyles';
+
+function ShowStars({star}){
+  var starImages = [];
+  for(let i= 0; i<star; i++){
+      starImages.push(<Image  style={styles.stars} source={require('../assets/img/star.jpg')}/>)    
+  }
+  return <View style={styles.starImages}>{starImages}</View>
+}
+
 
 export default function ReviewDetails({navigation}) {
     // const pressHandler = () =>{
     //     navigation.goBack();
     // }
+    
+
   return (
-    <View style={globalStyles.container}>
+    <ImageBackground source={require('../assets/img/bgImg.png')} style={globalStyles.container}>
       <Text>Review Details Screen</Text>
-      <Text>{navigation.getParam('title')}</Text>
+      <Text>{navigation.getParam('title')}</Text> 
       <Text>{navigation.getParam('body')}</Text>
-      <Text>{navigation.getParam('rating')}</Text>
-    </View>
+      <ShowStars star={navigation.getParam('rating')}/>
+      {/* <Text>{navigation.getParam('rating')}</Text> */}
+    </ImageBackground>
   );
 }
+
+const styles = StyleSheet.create({
+  starImages:{
+     flexDirection:'row' 
+  },
+  stars: {
+    width: 50,
+    height: 50,
+  },
+  
+});
